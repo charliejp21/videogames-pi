ad41c11b6856f5a69d56161a5656638e03a388a3
charliejp21 <charliejp21@gmail.com>
2023-07-03 20:56:22 -0600

    Deploy en reander

--- api/src/db.js
+++ api/src/db.js
@@ -2,18 +2,19 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLYOY
-} = process.env;
+const { DB_DEPLOY} = process.env;
 
-/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-}); */
+  dialectOptions: {
 
-const sequelize = (DB_DEPLYOY , {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    ssl: {
+
+      require: true
+
+    }
+  }
 });
 
 const basename = path.basename(__filename);
